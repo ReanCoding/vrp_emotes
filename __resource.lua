48f40414a8dcf3566085bf8ab643d19f99de9841
ReanCoding <48993100+ReanCoding@users.noreply.github.com>
2019-04-25 01:13:31 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,6 +1,6 @@
 --[[
     Lavet af: ReanCoding
 --]]
-resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937' 
 
 client_script "penge.lua"
