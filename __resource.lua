6d9e0b646328ef46f15cdb818a03c203c0d7843d
ReanCoding <48993100+ReanCoding@users.noreply.github.com>
2019-04-25 02:15:24 +0200

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -4,10 +4,11 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 server_script{  
-"sv.lua"
+"server.lua"
 }
 
 client_script{  
+"client.lua",    
 "penge.lua",
 "fyrværkeri.lua"
 }
