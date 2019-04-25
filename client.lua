42477b5f53efd40a1682d23d2285848c35c53291
ReanCoding <48993100+ReanCoding@users.noreply.github.com>
2019-04-25 02:17:48 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -1,1 +1,33 @@
 
+--[[
+    Lavet af: ReanCoding
+--]]
+
+------------------------------------------------ Scriptet -----------------------------------------------------------------
+------------------------------------------------ Du må ikke ændre noget her -----------------------------------------------------------------
+local ReanCoding = {
+    ['penge'] = "penge",
+    ['fyr'] = "fyr"
+}
+
+RegisterNetEvent("Emotes:ReanCoding")
+function displayEmotes()
+    local index = 0
+    local display = "^7"
+
+    for name, value in pairs(ReanCoding) do
+        index = index + 1
+        if index == 1 then
+            display = display..name
+        else
+            display = display..", "..name
+        end
+    end
+
+    TriggerEvent("chatMessage", "EMOTES", {255,0,0}, "")
+    TriggerEvent("chatMessage", "", {255,255,255}, "penge - fyr")
+end
+
+AddEventHandler("Emotes:ReanCoding", function()
+    displayEmotes()
+end)
